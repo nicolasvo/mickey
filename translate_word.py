6416ddd1259f133a162a4bc9bf4b396f7e6f3f74
Nicolas <nicolasvo95@gmail.com>
2018-09-10 20:56:41 +0200

    update loop

--- translate_word.py
+++ translate_word.py
@@ -1,11 +1,13 @@
 import json
 from googletrans import Translator
+from magic_logger import MagicLogger
+
+logger = MagicLogger("Translation script")
 
 def get_translation(word, language_destination, language_source="en"):
 
     translator = Translator()
     word_translated = translator.translate(word, dest=language_destination, src=language_source).text
-    print(word, ">", word_translated)
 
     return word_translated if not word_translated == word.capitalize() else None
 
@@ -19,10 +21,22 @@ dict_translation = {
     "en": word_english,
 }
 
-for key in dict_language.keys():
-    dict_translation[key] = [get_translation(word, key) for word in dict_translation["en"]]
-    with open("words.json", "w") as f:
-        json.dump(dict_translation, f)
+#for key in dict_language.keys():
+#    dict_translation[key] = [get_translation(word, key) for word in dict_translation["en"]]
+#    with open("words.json", "w") as f:
+#        json.dump(dict_translation, f)
+#
+#with open("words.json", "w") as f:
+#    json.dump(dict_translation, f)
 
-with open("words.json", "w") as f:
-    json.dump(dict_translation, f)
+list_word = []
+
+for word in word_english:
+    dict_word = {"en": word}
+    for language in dict_language.keys():
+        word_translated = get_translation(word, language)
+        dict_word.update({language: word_translated})
+        logger.info(msg=f"{word}: {word_translated}, {language}")
+    list_word.append(dict_word)
+    with open("words.json", "w") as f:
+        json.dump(list_word, f)
